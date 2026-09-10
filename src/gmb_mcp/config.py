2c5e92c7004d36c20e3ee361418399f3da71761c
clipskills <clipskills@gmail.com>
2026-09-10 13:02:05 +0200

    Update config.py

--- src/gmb_mcp/config.py
+++ src/gmb_mcp/config.py
@@ -7,34 +7,25 @@ DEFAULT_LOG_LEVEL = "INFO"
 
 GOOGLE_OAUTH_SCOPES = [
     "https://www.googleapis.com/auth/business.manage",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive.metadata.readonly",
 ]
-
 
 def get_env(name, default=""):
     return os.getenv(name, default).strip()
 
-
 def get_google_client_id():
     return get_env("GOOGLE_CLIENT_ID")
-
 
 def get_google_client_secret():
     return get_env("GOOGLE_CLIENT_SECRET")
 
-
 def get_google_redirect_uri():
     return get_env("GOOGLE_REDIRECT_URI", DEFAULT_REDIRECT_URI)
-
 
 def get_google_auth_uri():
     return get_env("GOOGLE_AUTH_URI", DEFAULT_AUTH_URI)
 
-
 def get_google_token_uri():
     return get_env("GOOGLE_TOKEN_URI", DEFAULT_TOKEN_URI)
-
 
 def get_log_level():
     return get_env("LOG_LEVEL", DEFAULT_LOG_LEVEL).upper()
